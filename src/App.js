759c0ab6dab1a59f67db33ff54a2f76b042bf641
Raul Fiori <raulproc1807@gmail.com>
2019-05-29 11:29:12 -0300

    initial setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <h1> Hello, World! </h1>
+    <p>shiiit</p>
+  </div>
+);
+
+export default hot(module)(App);
